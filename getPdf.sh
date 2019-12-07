e58187ac282dec2a7d3dbe4192fd9e743b19b9c5
Raptor <pierre.blank@epitech.eu>
2019-12-07 17:49:54 +0100

    Added part-time Infflux (and paramtrized get pdf)

--- getPdf.sh
+++ getPdf.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-wkhtmltopdf https://helixire.github.io/\?lang\=fr tmp.pdf && pdftk tmp.pdf cat 1 output cv_fr.pdf
-wkhtmltopdf https://helixire.github.io/\?lang\=en tmp.pdf && pdftk tmp.pdf cat 1 output cv_en.pdf
+URL=https://helixire.github.io/
+
+if [[ $1 ]]; then
+  URL=$1
+fi
+
+wkhtmltopdf $URL\?lang\=fr tmp.pdf && pdftk tmp.pdf cat 1 output cv_fr.pdf
+wkhtmltopdf $URL\?lang\=en tmp.pdf && pdftk tmp.pdf cat 1 output cv_en.pdf
 rm tmp.pdf
